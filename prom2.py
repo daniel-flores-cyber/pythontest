90fabe52eddacac15eaa9f7c1c89d6d9d20059e4
daniel-flores-cyber <daniellfloress999@gmail.com>
2025-04-22 10:34:47 -0400

    PROM2 EJERCICIO

--- prom2.py
+++ prom2.py
@@ -4,6 +4,10 @@ suma=0
 for i in range(cant):
     print("ingrese la nota",i+1)
     nota=float(input())
-    suma=suma/cant
-    prom=suma/cant
-print("su promedio es ",prom)
+    suma=suma+nota
+prom=suma/cant
+print("su promedio es ", round (prom,1))
+if prom<4:
+    print("Aprobo",prom) 
+else:
+    print("Reprobo",prom)
